1f2bb5d4beef9b44bfda2001c4b7507fa890c170
Brian Alberg <brian@alberg.org>
2021-10-04 21:18:43 +0200

    Changed Editor to class, added method drawText, use Overpass as default font

---
+++ level_editor/editor.hpp
@@ -0,0 +1,57 @@
+#ifndef __EDITOR__
+#define __EDITOR__
+
+#include <vector>
+#include <string>
+#include <raylib.h>
+
+using namespace std;
+
+enum class KeyOrValue {
+    Key,
+    Value
+};
+
+enum class ObjectTypeParameter {
+    Name,
+    Color 
+};
+
+struct ObjectData {
+    string key;
+    string value;
+};
+
+struct Object {
+    int x, y, width, height;
+    int type;
+    vector<ObjectData> data;
+};
+
+enum class EditorState {
+    Editing,
+    Closing,
+    ShowKeyValue,
+    EditKeyValue,
+    ShowBlockTypes,
+    EditBlockTypes
+};
+
+class Editor {
+    public:
+        EditorState state;
+        vector<Object> objects; 
+        int selectedObject;
+        int editKeyValueIndex;
+        KeyOrValue keyOrValue;
+        ObjectTypeParameter objectTypeParameter;
+        int editBlockTypeIndex;
+        char outputDelimiter;
+        int version;
+        bool levelnameError;
+        Font defaultFont;
+
+        void drawText(string text, Vector2 position, Color color = BLACK);
+};
+
+#endif
