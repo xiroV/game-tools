38f1621c0fd847c1b5b21ae1ab0e28e3c9b0f671
Jonas Malte Hinchely <jonas@hinchely.dk>
2022-10-03 12:04:36 +0200

    Update main

--- LevelEditorSwift/Sources/LevelEditorSwift/main.swift
+++ LevelEditorSwift/Sources/LevelEditorSwift/main.swift
@@ -1,31 +1,97 @@
 import Raylib
 
-var screenWidth = 1000
-var screenHeight = 800
+func loadLevel(_ editor: Editor) {
+    // TODO Not yet implemented
+}
+
+func control() {
+//func control(_ editor: Editor, windows: Window, exporters: Exporters) {
+    // TODO Not yet implemented
+}
+
+func drawMessages() {
+
+}
 
 func main() {
     var exit = false
 
-    Raylib.initWindow(800, 1000, "Level Editor")
-    Raylib.setTargetFPS(60)
-    
-    var qc = QuakeConsole()
+    let window = Window(
+        width: 1600,
+        height: 1000,
+        title: "Level Editor"
+    )
 
-    Raylib.setExitKey(.function10)
+    let editor = Editor(
+        version: 1,
+        outputDelimiter: ";",
+        window: window,
+        fontSize: 20
+    )
+
+
+    Raylib.initWindow(editor.window.width, editor.window.height, editor.window.title)
+    Raylib.setTargetFPS(60)
+
+    Raylib.setExitKey(.null)
+
+    // TODO(Brian) Figure font out later
+    //let fontDefault = Raylib.loadFontEx("assets/fonts/OverpassMono/OverpassMono-Regular.ttf", editor.fontSize, 0, 0);
+    //SetTextureFilter(fontDefault.texture, TEXTURE_FILTER_BILINEAR);
+    //GuiSetFont(fontDefault);
+
+    if (CommandLine.arguments.count > 1) {
+        editor.levelName = CommandLine.arguments.first!
+        loadLevel(editor)
+    } else {
+        editor.levelName = "TestLevel1.lvl"
+        loadLevel(editor)
+    }
+
+    var camera = Camera2D() 
+    camera.target = Vector2(x: Float(editor.window.width)/2, y: Float(editor.window.height)/2);
+    camera.offset = Vector2(x: Float(editor.window.width)/2, y: Float(editor.window.height)/2);
+    camera.rotation = 0.0;
+    camera.zoom = 1.0;
+
+    if (editor.objects.count > 0) {
+        editor.cameraTarget = Vector2(x: Float(editor.objects[0].x), y: Float(editor.objects[0].y));
+    }
 
     while !exit {
+        //control(editor, windows, exporters)
+
+        /*
+        camera.zoom += cameraZoom;
+        if (camera.zoom > 2.0f) {
+            cameraZoom = 2.0f;
+            camera.zoom = cameraZoom;
+        }
+        else if (camera.zoom < 0.5f) {
+            cameraZoom = 0.5f;
+            camera.zoom = cameraZoom;
+        }
+
+        camera.target = editor.cameraTarget;
+        */
+
         Raylib.beginDrawing()
-            Raylib.clearBackground(.gray)
-            Raylib.drawText("Hello", 10, 10, 1, .black)
-            qc.render(screenWidth: screenWidth, screenHeight: screenHeight)
+            Raylib.clearBackground(.darkGray)
+            //drawGrid(editor, camera);
+            Raylib.beginMode2D(camera);
+                //drawObjects(&camera, &editor);
+            Raylib.endMode2D();
+            //drawHelp(&editor);
+            //drawWindows(&editor, &windows, exporters)
+            if (editor.showFPS) { Raylib.drawFPS(20, 20) }
+            //drawMessages(&editor);
         Raylib.endDrawing()
 
-        qc.update()
-        if !qc.isOpen {
-            if Raylib.isKeyPressed(.escape) {
-                
-                exit = true
-            }
+        camera.zoom = 0
+        //updateEditor(&editor);
+
+        if Raylib.isKeyPressed(.escape) {
+            exit = true
         }
     }
 
