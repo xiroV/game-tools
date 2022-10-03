5299a8b4381b69617f78145eadf52a232a3551b0
Jonas Malte Hinchely <jonas@hinchely.dk>
2022-10-03 17:00:36 +0200

    Some QC work

--- LevelEditorSwift/Sources/LevelEditorSwift/quake_console.swift
+++ LevelEditorSwift/Sources/LevelEditorSwift/quake_console.swift
@@ -3,11 +3,11 @@ import Raylib
 let ConsoleHeightPercent: Float32 = 0.20;
 
 class QuakeConsole {
-    var isOpen: Bool = false;
-    var openT: Float32 = 0.0;
-    var currentCommand: [UInt8] = [];
-    var history: [String];
-    var currentHistoryEntry = -1;
+    var isOpen: Bool = false
+    var openT: Float32 = 0.0
+    var currentCommand: [Character] = []
+    var history: [String]
+    var currentHistoryEntry = -1
     
     init() {
         self.isOpen = false
@@ -18,127 +18,123 @@ class QuakeConsole {
     }
 
     func update() {
-        let dt: Float32 = Raylib.getFrameTime();
+        let dt: Float32 = Raylib.getFrameTime()
         if self.isOpen {
-            self.openT += dt * 3;
+            self.openT += dt * 3
         } else {
-            self.openT -= dt * 3;
+            self.openT -= dt * 3
         }
         if self.openT < 0.0 {
-            self.openT = 0.0;
+            self.openT = 0.0
         } else if self.openT > 1.0 {
-            self.openT = 1;
+            self.openT = 1
         }
     }
 
     func input() {
         if Raylib.isKeyPressed(.grave) {
-            self.isOpen = !self.isOpen;
-            return;
+            self.isOpen = !self.isOpen
+            return
         }
 
         if !self.isOpen {return;}
 
         if Raylib.isKeyPressed(.enter) {
-            if self.currentCommand[0] != 0 {
-                let asString = String(decoding: self.currentCommand, as: UTF8.self)
-                self.history.append(asString);
-                self.currentCommand[0] = 0;
-                self.parseAndExecuteCommand();
-                self.currentHistoryEntry = -1;
-                return;
+            if self.currentCommand.count != 0 {
+                let asString = String(self.currentCommand)
+                self.history.append(asString)
+                self.currentCommand.removeAll()
+                self.parseAndExecuteCommand()
+                self.currentHistoryEntry = -1
+                return
             }
         }
 
         if Raylib.isKeyPressed(.up) {
-            self.currentHistoryEntry += 1;
-            self.currentHistoryEntry = clamp(self.currentHistoryEntry, -1, self.history.count - 1);
+            self.currentHistoryEntry += 1
+            self.currentHistoryEntry = clamp(self.currentHistoryEntry, -1, self.history.count - 1)
             if self.currentHistoryEntry != -1 {
-                moveStringToCurrentCommand(command: self.history[self.history.count - self.currentHistoryEntry - 1]);
+                moveStringToCurrentCommand(command: self.history[self.history.count - self.currentHistoryEntry - 1])
             }
         }
 
         if Raylib.isKeyPressed(.down) {
-            self.currentHistoryEntry -= 1;
-            self.currentHistoryEntry = clamp(self.currentHistoryEntry, -1, self.history.count - 1);
+            self.currentHistoryEntry -= 1
+            self.currentHistoryEntry = clamp(self.currentHistoryEntry, -1, self.history.count - 1)
             if self.currentHistoryEntry != -1 {
-                moveStringToCurrentCommand(command: self.history[self.history.count - self.currentHistoryEntry - 1]);
+                moveStringToCurrentCommand(command: self.history[self.history.count - self.currentHistoryEntry - 1])
             }
         }
 
 
-        guard var key = Raylib.getCharPressed()?.wholeNumberValue else {
-            return;
+        guard var key = Raylib.getCharPressed() else {
+            return
         }
         
-        while key > 0 {
-            if (key >= 32) && (key <= 125) {
-                var counter = 0;
-                while self.currentCommand[counter] != 0 && counter < self.currentCommand.count - 1 {counter += 1;}
-                if counter != self.currentCommand.count - 1 {
-                    self.currentCommand[counter] = UInt8(key);
-                    self.currentCommand[counter + 1] = 0;
-                }
+        guard var keyValue = key.asciiValue else {
+            return
+        }
+        
+        while keyValue > 0 {
+            if keyValue >= 32 && keyValue <= 125 {
+                self.currentCommand.append(key)
             }
             self.currentHistoryEntry = -1;
             
-            guard let newKey = Raylib.getCharPressed()?.wholeNumberValue else {
-                return;
+            guard let newKey = Raylib.getCharPressed() else {
+                return
+            }
+            
+            guard let newKeyValue = newKey.asciiValue else {
+                return
             }
             
             key = newKey;
+            
+            keyValue = newKeyValue
         }
 
         if Raylib.isKeyPressed(.backspace) {
-            self.currentHistoryEntry = -1;
-            var counter = 0;
-            while self.currentCommand[counter] != 0 && counter < self.currentCommand.count + 1 {counter += 1}
-            if counter != 0 {
-                self.currentCommand[counter - 1] = 0;
-            }
+            _ = self.currentCommand.popLast()
         }
-
     }
 
-    func render(screenWidth: Int, screenHeight: Int) {
-        var bottom = (ConsoleHeightPercent * Float(screenHeight) * self.openT)
-        Raylib.drawRectangle(0, 0, Int32(screenWidth), Int32(bottom), .brown)
+    func render(window: Window) {
+        var bottom = (ConsoleHeightPercent * Float(window.height) * self.openT)
+        Raylib.drawRectangle(0, 0, Int32(window.width), Int32(bottom), .brown)
         bottom -= 28
         let isEmpty = self.currentCommand.isEmpty
-        Raylib.drawText(isEmpty ? "Start typing..." : String(decoding: self.currentCommand, as: UTF8.self), 4, Int32(bottom), 24, isEmpty ? .gray : .white);
+        Raylib.drawText(isEmpty ? "Start typing..." : String(self.currentCommand), 4, Int32(bottom), 24, isEmpty ? .gray : .white)
+        if self.history.count == 0 {return}
         for it in 1...self.history.count  {
-            bottom -= 28;
-            let index = self.history.count - it;
-            Raylib.drawText(self.history[index], 4, Int32(bottom), 24, index == (self.history.count - self.currentHistoryEntry - 1) ? .blue : .gray);
+            bottom -= 28
+            let index = self.history.count - it
+            Raylib.drawText(self.history[index], 4, Int32(bottom), 24, index == (self.history.count - self.currentHistoryEntry - 1) ? .blue : .gray)
         }
     }
 
     func parseAndExecuteCommand() {
         let tokenized = self.history[self.history.count - 1].split(separator: " ");
-        if tokenized.count == 0 {return;} // Shouldn't be possible.
-        let first = tokenized[0];
+        if tokenized.count == 0 {return} // Shouldn't be possible, but let's handle it.
+        let first = tokenized[0]
         switch first {
             case "clear_history":
-                self.history = [];
+                self.history = []
             default:
-                ();
+                ()
         }
     }
 
     func moveStringToCurrentCommand(command: String) {
-        for it in 0...command.count - 1 {
-            // self.currentCommand[it] = command[it]?.wholeNumberValue;
-            _ = it;
-            (/* TODO */);
-            
+        self.currentCommand.removeAll()
+        for ch in command {
+            self.currentCommand.append(ch)
         }
-        
-        self.currentCommand[command.count] = 0;
     }
 };
 
 func clamp(_ input: Int, _ lower: Int, _ upper: Int) -> Int {
-    if input < lower { return lower; }
-    if input > upper { return upper; }
-    return input;
+    if input < lower { return lower }
+    if input > upper { return upper }
+    return input
 }
