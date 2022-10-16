9e7a2ebf6db5cdb9186fca4325a961cc7ab512db
Brian Ravn <brian@xirov.com>
2022-10-16 19:46:13 +0200

    Added dependency to SwiftRaylibGui

--- LevelEditorSwift/Package.swift
+++ LevelEditorSwift/Package.swift
@@ -8,14 +8,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/STREGAsGate/Raylib.git", branch: "master")
+        .package(url: "https://github.com/STREGAsGate/Raylib.git", branch: "master"),
+        .package(url: "git@github.com:xiroV/SwiftRaylibGui.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "LevelEditorSwift",
-            dependencies: ["Raylib"]),
+            dependencies: ["Raylib", "SwiftRaylibGui"]),
         .testTarget(
             name: "LevelEditorSwiftTests",
             dependencies: ["LevelEditorSwift"]),
