3a30c70adc9e809251bde3e364779cf958618aa3
Brian Ravn <brian@xirov.com>
2023-05-24 00:30:49 +0200

    Export json version as valid json

--- level_editor/export/json.hpp
+++ level_editor/export/json.hpp
@@ -10,24 +10,24 @@ struct JsonExporter : Exporter {
     }
 
     // create string with i spaces
-    std::string s(int i) {
-        return std::string(i, ' ');
+    string s(int i) {
+        return string(i, ' ');
     }
 
-    std::string generate(Editor* editor) {
-        std::string output;
+    string generate(Editor* editor) {
+        string output;
 
-        output += "#version " + std::to_string(editor->version) + "\n";
         output += "{\n";
+        output += s(2) + "\"version\": " + to_string(editor->version) + ",\n";
         output += s(2) + "\"objects\": [\n";
 
         for (unsigned int i = 0; i < editor->objects.size(); i++) {
             output += s(4) + "{\n";
-            output += s(6) + "\"x\": " + std::to_string(editor->objects[i].x) + ",\n";
-            output += s(6) + "\"y\": " + std::to_string(editor->objects[i].y) + ",\n";
-            output += s(6) + "\"width\": " + std::to_string(editor->objects[i].width) + ",\n";
-            output += s(6) + "\"height\": " + std::to_string(editor->objects[i].height) + ",\n";
-            output += s(6) + "\"rotation\": " + std::to_string(editor->objects[i].rotation) + ",\n";
+            output += s(6) + "\"x\": " + to_string(editor->objects[i].x) + ",\n";
+            output += s(6) + "\"y\": " + to_string(editor->objects[i].y) + ",\n";
+            output += s(6) + "\"width\": " + to_string(editor->objects[i].width) + ",\n";
+            output += s(6) + "\"height\": " + to_string(editor->objects[i].height) + ",\n";
+            output += s(6) + "\"rotation\": " + to_string(editor->objects[i].rotation) + ",\n";
             output += s(6) + "\"type\": \"" + editor->objectTypes[editor->objects[i].type].name + "\",\n";
 
             if (editor->objects[i].data.size() < 1) {
